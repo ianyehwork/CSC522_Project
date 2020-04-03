8f263973c9837aaa7afd71b38c52c03bb0a7bf63
TSU HSIN YEH <tyeh3@IDHFJSNF.local>
2020-04-02 20:11:48 -0400

    1. Added crew dataset 2. extract feature for director popularity and vote score.

--- data.py
+++ data.py
@@ -12,6 +12,7 @@ pd.options.mode.chained_assignment = None
 # Import the dataset (assuming the working directory is CSC522Project)
 # credits_data = credits_data[credits_data['id'].isin(set(movies_meta_data['id'].unique()))]
 # credits_data.to_csv('data/casts.csv', index=False)
+crews_data = pd.read_csv('data/crews.csv')
 casts_data = pd.read_csv('data/casts.csv')
 keywords_data = pd.read_csv('data/keywords.csv')
 movies_meta_data = pd.read_csv('data/movies_metadata.csv')
@@ -33,23 +34,24 @@ movies_meta_data['release_date']= pd.to_datetime(movies_meta_data['release_date'
 
 # Select the movies with both revenue and budget greater than 0
 movies_meta_data = movies_meta_data[((movies_meta_data.loc[:,'budget'] > 0) & (movies_meta_data.loc[:,'revenue'] > 0))]
-# Calculate return of investment attributes
-movies_meta_data['return_of_investment'] = ((movies_meta_data['revenue'] - movies_meta_data['budget']) / movies_meta_data['budget'])
-# movies_meta_data = movies_meta_data.drop(columns=['revenue', 'budget'])
-
 # Remove the movie with runtime equals 0
 movies_meta_data = movies_meta_data[movies_meta_data['runtime'] > 0]
+# Calculate return of investment attributes
+movies_meta_data['return_on_investment'] = ((movies_meta_data['revenue'] - movies_meta_data['budget']) / movies_meta_data['budget'])
+# movies_meta_data = movies_meta_data.drop(columns=['revenue', 'budget'])
 
 # Remove the outliers using z-score
 movies_meta_data = movies_meta_data[(np.abs(stats.zscore(movies_meta_data.budget)) <= 3)]
 movies_meta_data = movies_meta_data[(np.abs(stats.zscore(movies_meta_data.revenue)) <= 3)]
-movies_meta_data = movies_meta_data[(np.abs(stats.zscore(movies_meta_data.return_of_investment)) <= 3)]
+movies_meta_data = movies_meta_data[(np.abs(stats.zscore(movies_meta_data.return_on_investment)) <= 3)]
  
 # Merge with the keywords and credits
 movies_meta_data = movies_meta_data.merge(keywords_data, left_on='id', right_on='id')
 movies_meta_data = movies_meta_data.merge(casts_data, left_on='id', right_on='id')
+movies_meta_data = movies_meta_data.merge(crews_data, left_on='id', right_on='id')
 del(keywords_data)
 del(casts_data)
+del(crews_data)
 
 # Drop the unnecessary column
 movies_meta_data = movies_meta_data.drop(columns=['id','budget','revenue','vote_count'])
@@ -83,6 +85,17 @@ for cast in [eval(casts) for casts in movies_meta_data['cast']]:
 movies_meta_data['cast'] = [[c['name'] for c in cast] for cast in [eval(casts) for casts in movies_meta_data['cast']]]
 del(cast)
 
+directors_name_count_map = dict()
+directors_name_total_pop_map = dict()
+directors_name_total_vote_map = dict()
+for crew in [eval(crews) for crews in movies_meta_data['crew']]:
+    for c in crew:
+        if c['job'] == 'Director':
+            directors_name_count_map.update({c['name']: directors_name_count_map.get(c['name'], 0) + 1})
+movies_meta_data['directors'] = [[c['name'] for c in crew if c['job'] == 'Director'] for crew in [eval(crews) for crews in movies_meta_data['crew']]]
+movies_meta_data = movies_meta_data.drop(columns=['crew'])
+del(crew)
+
 for i in movies_meta_data.index: 
      for g in movies_meta_data['genres'][i]:
          genres_name_total_pop_map.update({g: genres_name_total_pop_map.get(g, 0) + movies_meta_data['popularity'][i]})
@@ -93,6 +106,9 @@ for i in movies_meta_data.index:
      for c in movies_meta_data['cast'][i]:
          casts_name_total_pop_map.update({c: casts_name_total_pop_map.get(c, 0) + movies_meta_data['popularity'][i]})
          casts_name_total_vote_map.update({c: casts_name_total_vote_map.get(c, 0) + movies_meta_data['vote_average'][i]})
+     for d in movies_meta_data['directors'][i]:
+         directors_name_total_pop_map.update({d: directors_name_total_pop_map.get(d, 0) + movies_meta_data['popularity'][i]})
+         directors_name_total_vote_map.update({d: directors_name_total_vote_map.get(d, 0) + movies_meta_data['vote_average'][i]})
 
 genres_name_avg_pop_map = dict()
 genres_name_avg_vote_map = dict()
@@ -100,6 +116,8 @@ keywords_name_avg_pop_map = dict()
 keywords_name_avg_vote_map = dict()
 casts_name_avg_pop_map = dict()
 casts_name_avg_vote_map = dict()
+directors_name_avg_pop_map = dict()
+directors_name_avg_vote_map = dict()
 for g in genres_name_count_map:
     genres_name_avg_pop_map.setdefault(g, genres_name_total_pop_map.get(g) / genres_name_count_map.get(g))
     genres_name_avg_vote_map.setdefault(g, genres_name_total_vote_map.get(g) / genres_name_count_map.get(g))
@@ -109,17 +127,23 @@ for k in keywords_name_count_map:
 for c in casts_name_count_map:
     casts_name_avg_pop_map.setdefault(c, casts_name_total_pop_map.get(c) / casts_name_count_map.get(c))
     casts_name_avg_vote_map.setdefault(c, casts_name_total_vote_map.get(c) / casts_name_count_map.get(c))
-del(i, g, k, c)
+for d in directors_name_count_map:
+    directors_name_avg_pop_map.setdefault(d, directors_name_total_pop_map.get(d) / directors_name_count_map.get(d))
+    directors_name_avg_vote_map.setdefault(d, directors_name_total_vote_map.get(d) / directors_name_count_map.get(d))
+del(i, g, k, c, d)
 del(genres_name_count_map, genres_name_total_pop_map, genres_name_total_vote_map)
 del(keywords_name_count_map, keywords_name_total_pop_map, keywords_name_total_vote_map)
 del(casts_name_count_map, casts_name_total_pop_map, casts_name_total_vote_map)
+del(directors_name_count_map, directors_name_total_pop_map, directors_name_total_vote_map)
 
-movies_meta_data['genres_popularity'] = 0.0
-movies_meta_data['genres_vote'] = 0.0
-movies_meta_data['keywords_popularity'] = 0.0
-movies_meta_data['keywords_vote'] = 0.0
-movies_meta_data['casts_popularity'] = 0.0
-movies_meta_data['casts_vote'] = 0.0
+movies_meta_data['genres_popularity_score'] = 0.0
+movies_meta_data['genres_vote_score'] = 0.0
+movies_meta_data['keywords_popularity_score'] = 0.0
+movies_meta_data['keywords_vote_score'] = 0.0
+movies_meta_data['casts_popularity_score'] = 0.0
+movies_meta_data['casts_vote_score'] = 0.0
+movies_meta_data['directors_popularity_score'] = 0.0
+movies_meta_data['directors_vote_score'] = 0.0
 for i in movies_meta_data.index: 
     # genres
     g_pop_sum = 0
@@ -130,11 +154,11 @@ for i in movies_meta_data.index:
         g_vote_sum = g_vote_sum + genres_name_avg_vote_map.get(g)
         g_count = g_count + 1
     if g_count == 0:
-        movies_meta_data['genres_popularity'][i] = 0
-        movies_meta_data['genres_vote'][i] = 0
+        movies_meta_data['genres_popularity_score'][i] = 0
+        movies_meta_data['genres_vote_score'][i] = 0
     else:
-        movies_meta_data['genres_popularity'][i] = g_pop_sum / g_count
-        movies_meta_data['genres_vote'][i] = g_vote_sum / g_count
+        movies_meta_data['genres_popularity_score'][i] = g_pop_sum / g_count
+        movies_meta_data['genres_vote_score'][i] = g_vote_sum / g_count
     
     # keywords
     k_pop_sum = 0
@@ -145,11 +169,11 @@ for i in movies_meta_data.index:
         k_vote_sum = k_vote_sum + keywords_name_avg_vote_map.get(k)
         k_count = k_count + 1 
     if k_count == 0:
-        movies_meta_data['keywords_popularity'][i] = 0
-        movies_meta_data['keywords_vote'][i] = 0
+        movies_meta_data['keywords_popularity_score'][i] = 0
+        movies_meta_data['keywords_vote_score'][i] = 0
     else:
-        movies_meta_data['keywords_popularity'][i] = k_pop_sum / k_count
-        movies_meta_data['keywords_vote'][i] = k_vote_sum / k_count
+        movies_meta_data['keywords_popularity_score'][i] = k_pop_sum / k_count
+        movies_meta_data['keywords_vote_score'][i] = k_vote_sum / k_count
     
     # casts
     c_pop_sum = 0
@@ -160,11 +184,36 @@ for i in movies_meta_data.index:
         c_vote_sum = c_vote_sum + casts_name_avg_vote_map.get(c)
         c_count = c_count + 1 
     if c_count == 0:
-        movies_meta_data['casts_popularity'][i] = 0
-        movies_meta_data['casts_vote'][i] = 0
+        movies_meta_data['casts_popularity_score'][i] = 0
+        movies_meta_data['casts_vote_score'][i] = 0
     else:
-        movies_meta_data['casts_popularity'][i] = c_pop_sum / c_count
-        movies_meta_data['casts_vote'][i] = c_vote_sum / c_count
+        movies_meta_data['casts_popularity_score'][i] = c_pop_sum / c_count
+        movies_meta_data['casts_vote_score'][i] = c_vote_sum / c_count
+    
+    # directors
+    d_pop_sum = 0
+    d_vote_sum = 0
+    d_count = 0
+    for d in movies_meta_data['directors'][i]:
+        d_pop_sum = d_pop_sum + directors_name_avg_pop_map.get(d)
+        d_vote_sum = d_vote_sum + directors_name_avg_vote_map.get(d)
+        d_count = d_count + 1 
+    if d_count == 0:
+        movies_meta_data['directors_popularity_score'][i] = 0
+        movies_meta_data['directors_vote_score'][i] = 0
+    else:
+        movies_meta_data['directors_popularity_score'][i] = d_pop_sum / d_count
+        movies_meta_data['directors_vote_score'][i] = d_vote_sum / d_count
+
+del(i)
+del(c, c_count, c_pop_sum, c_vote_sum)
+del(g, g_count, g_pop_sum, g_vote_sum)
+del(k, k_count, k_pop_sum, k_vote_sum)
+del(d, d_count, d_pop_sum, d_vote_sum)
+del(genres_name_avg_pop_map, genres_name_avg_vote_map)
+del(keywords_name_avg_pop_map, keywords_name_avg_vote_map)
+del(casts_name_avg_pop_map, casts_name_avg_vote_map)
+del(directors_name_avg_pop_map, directors_name_avg_vote_map)
 
 # Remove the outliers using z-score
 movies_meta_data = movies_meta_data[(np.abs(stats.zscore(movies_meta_data.genres_popularity)) <= 3)]
@@ -173,17 +222,10 @@ movies_meta_data = movies_meta_data[(np.abs(stats.zscore(movies_meta_data.keywor
 movies_meta_data = movies_meta_data[(np.abs(stats.zscore(movies_meta_data.keywords_vote)) <= 3)]
 movies_meta_data = movies_meta_data[(np.abs(stats.zscore(movies_meta_data.casts_popularity)) <= 3)]
 movies_meta_data = movies_meta_data[(np.abs(stats.zscore(movies_meta_data.casts_vote)) <= 3)]
-
-del(c, c_count, c_pop_sum, c_vote_sum)
-del(g, g_count, g_pop_sum, g_vote_sum)
-del(k, k_count, k_pop_sum, k_vote_sum)
-del(i)
-del(genres_name_avg_pop_map, genres_name_avg_vote_map, keywords_name_avg_pop_map, keywords_name_avg_vote_map, casts_name_avg_pop_map, casts_name_avg_vote_map)
+movies_meta_data = movies_meta_data[(np.abs(stats.zscore(movies_meta_data.directors_popularity)) <= 3)]
+movies_meta_data = movies_meta_data[(np.abs(stats.zscore(movies_meta_data.directors_vote)) <= 3)]
 
 # movies_meta_data = movies_meta_data.drop(columns=['genres','keywords','cast','popularity','vote_average'])
+movies_meta_data = movies_meta_data.drop(columns=['genres','keywords','cast','directors','overview','tagline','popularity','vote_average'])
 movies_meta_data.info()
-
-# figure out when to do normalization? (return on investment?)
-# how many movies do not have tagline
-# correlation between all the columns
-# normalize the return_of_investment
+movies_meta_data.describe(include='all')
