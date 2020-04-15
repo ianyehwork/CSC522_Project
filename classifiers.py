47b3e50a2fe46121e22d039980f8aedc74ee32b6
xliu16 <32623183+xliu16@users.noreply.github.com>
2020-04-15 14:13:12 -0400

    change made on cast_pop/vote;kept the budget

--- classifiers.py
+++ classifiers.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sklearn
 from sklearn import tree
 from sklearn import preprocessing
+from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 import graphviz
 
@@ -15,11 +16,13 @@ levels = movies_meta_data['return_on_investment_label']
 x_train = movies_meta_data.copy()
 del x_train['return_on_investment']
 del x_train['return_on_investment_label']
+del x_train['Unnamed: 0']
 
 # Normalization?
-X_scaled = preprocessing.scale(x_train)
+# X_scaled = preprocessing.scale(x_train)
+
 # Split the dataset into train/test with 2/3 being training data and 1/3 being testing data.
-X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X_scaled, levels, shuffle=True)
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(x_train, levels, shuffle=True, test_size=0.2)
 
 # Decision Tree
 clf = tree.DecisionTreeClassifier(max_depth=8, min_samples_split=15)
@@ -28,6 +31,11 @@ y_pred = clf.predict(X_test)
 
 print("Accuracy of Decision Tree classifier: ")
 print(clf.score(X_test, y_test))
+cv_score = cross_val_score(clf, x_train, levels, cv=10)
+print("Accuracy of Decision Tree classifier after cross validation: ")
+print("Accuracy: %0.2f (+/- %0.2f)" % (cv_score.mean(), cv_score.std() * 2))
+
+
 
 dot_data = tree.export_graphviz(clf, out_file=None) 
 graph = graphviz.Source(dot_data) 
@@ -37,3 +45,7 @@ graph.render("movie_tree")
 clf_random_forest = RandomForestClassifier().fit(X_train, y_train)
 print("Accuracy of Random Forest classifier: ")
 print(clf_random_forest.score(X_test, y_test))
+
+print("Accuracy of Decision Tree classifier after cross validation: ")
+print(cross_val_score(clf_random_forest, x_train, levels, cv=10))
+
--- kmean.py
+++ kmean.py
@@ -13,57 +13,56 @@ roi_pos_df = movies_meta_data[movies_meta_data['return_on_investment'] > 0]
 roi_pos = movies_meta_data[movies_meta_data['return_on_investment'] > 0]['return_on_investment'].values.reshape(-1, 1)
 
 # Using the elbow method to find the optimal number of clusters
-wcss = []
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 1).fit(roi_pos)
-    wcss.append(kmeans.inertia_)
-plt.plot(range(1, 11), wcss)
-plt.title('The Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
+# wcss = []
+# for i in range(1, 11):
+#     kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 1).fit(roi_pos)
+#     wcss.append(kmeans.inertia_)
+# plt.plot(range(1, 11), wcss)
+# plt.title('The Elbow Method')
+# plt.xlabel('Number of clusters')
+# plt.ylabel('WCSS')
+# plt.show()
 
 # Fitting K-Means to the dataset
-optimal_clusters = 4
+optimal_clusters = 3
 kmeans = KMeans(n_clusters = optimal_clusters, init = 'k-means++', random_state = 1)
 roi_cluster = kmeans.fit_predict(roi_pos)
 
 # Include the values with negative ROI in the graph
-plt.scatter(roi_pos[roi_cluster == 0, 0], np.zeros(np.count_nonzero(roi_cluster == 0)), s = 10, c = 'red', label = 'Very Low')
-plt.scatter(roi_pos[roi_cluster == 1, 0], np.zeros(np.count_nonzero(roi_cluster == 1)), s = 10, c = 'yellow', label = 'Low')
-plt.scatter(roi_pos[roi_cluster == 2, 0], np.zeros(np.count_nonzero(roi_cluster == 2)), s = 10, c = 'green', label = 'Medium')
-plt.scatter(roi_pos[roi_cluster == 3, 0], np.zeros(np.count_nonzero(roi_cluster == 3)), s = 10, c = 'blue', label = 'High')
-plt.scatter(kmeans.cluster_centers_[:, 0], np.zeros(optimal_clusters), s = 50, c = 'black', label = 'Centroids')
-plt.title('Clusters of movies')
-plt.xlabel('Ruturn on investment')
-plt.legend()
-plt.show()
+# plt.scatter(roi_pos[roi_cluster == 0, 0], np.zeros(np.count_nonzero(roi_cluster == 0)), s = 10, c = 'red', label = 'Very Low')
+# plt.scatter(roi_pos[roi_cluster == 1, 0], np.zeros(np.count_nonzero(roi_cluster == 1)), s = 10, c = 'yellow', label = 'Low')
+# plt.scatter(roi_pos[roi_cluster == 2, 0], np.zeros(np.count_nonzero(roi_cluster == 2)), s = 10, c = 'green', label = 'Medium')
+# plt.scatter(roi_pos[roi_cluster == 3, 0], np.zeros(np.count_nonzero(roi_cluster == 3)), s = 10, c = 'blue', label = 'High')
+# plt.scatter(kmeans.cluster_centers_[:, 0], np.zeros(optimal_clusters), s = 50, c = 'black', label = 'Centroids')
+# plt.title('Clusters of movies')
+# plt.xlabel('Ruturn on investment')
+# plt.legend()
+# plt.show()
 
 movies_meta_data['return_on_investment_label'] = -1
 movies_meta_data['return_on_investment_label'][roi_pos_df.index] = roi_cluster
-movies_meta_data['return_on_investment_label'] = movies_meta_data['return_on_investment_label'].map({-1: 0, 0: 1, 1: 2, 2: 3, 3: 4})
+# movies_meta_data['return_on_investment_label'] = movies_meta_data['return_on_investment_label'].map({-1: 0, 0: 1, 1: 2, 2: 3, 3: 4})
+movies_meta_data['return_on_investment_label'] = movies_meta_data['return_on_investment_label'].map({-1: 0, 0: 1, 1: 2, 2: 3})
 
 #print level ranges
-print()
-print('level0')
-print('min: ' + str(min(movies_meta_data[movies_meta_data['return_on_investment_label'] == 0]['return_on_investment'])))
-print('max: ' + str(max(movies_meta_data[movies_meta_data['return_on_investment_label'] == 0]['return_on_investment'])))
+# print()
+# print('level0')
+# print('min: ' + str(min(movies_meta_data[movies_meta_data['return_on_investment_label'] == 0]['return_on_investment'])))
+# print('max: ' + str(max(movies_meta_data[movies_meta_data['return_on_investment_label'] == 0]['return_on_investment'])))
 
-print('level1')
-print('min: ' + str(min(movies_meta_data[movies_meta_data['return_on_investment_label'] == 1]['return_on_investment'])))
-print('max: ' + str(max(movies_meta_data[movies_meta_data['return_on_investment_label'] == 1]['return_on_investment'])))
+# print('level1')
+# print('min: ' + str(min(movies_meta_data[movies_meta_data['return_on_investment_label'] == 1]['return_on_investment'])))
+# print('max: ' + str(max(movies_meta_data[movies_meta_data['return_on_investment_label'] == 1]['return_on_investment'])))
 
-print('level2')
-print('min: ' + str(min(movies_meta_data[movies_meta_data['return_on_investment_label'] == 2]['return_on_investment'])))
-print('max: ' + str(max(movies_meta_data[movies_meta_data['return_on_investment_label'] == 2]['return_on_investment'])))
+# print('level2')
+# print('min: ' + str(min(movies_meta_data[movies_meta_data['return_on_investment_label'] == 2]['return_on_investment'])))
+# print('max: ' + str(max(movies_meta_data[movies_meta_data['return_on_investment_label'] == 2]['return_on_investment'])))
 
-print('level3')
-print('min: ' + str(min(movies_meta_data[movies_meta_data['return_on_investment_label'] == 3]['return_on_investment'])))
-print('max: ' + str(max(movies_meta_data[movies_meta_data['return_on_investment_label'] == 3]['return_on_investment'])))
+# print('level3')
+# print('min: ' + str(min(movies_meta_data[movies_meta_data['return_on_investment_label'] == 3]['return_on_investment'])))
+# print('max: ' + str(max(movies_meta_data[movies_meta_data['return_on_investment_label'] == 3]['return_on_investment'])))
 
-print('level4')
-print('min: ' + str(min(movies_meta_data[movies_meta_data['return_on_investment_label'] == 4]['return_on_investment'])))
-print('max: ' + str(max(movies_meta_data[movies_meta_data['return_on_investment_label'] == 4]['return_on_investment'])))
+# print('level4')
+# print('min: ' + str(min(movies_meta_data[movies_meta_data['return_on_investment_label'] == 4]['return_on_investment'])))
+# print('max: ' + str(max(movies_meta_data[movies_meta_data['return_on_investment_label'] == 4]['return_on_investment'])))
 
-
-del(i, optimal_clusters, roi_cluster, roi_pos_df, roi_pos, wcss)
