0845b0bdeb601ae74e8341c898f98ca9139857b8
xliu16 <32623183+xliu16@users.noreply.github.com>
2020-04-17 01:35:24 -0400

    hyperparams tuning done; included code of plotting

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,6 @@
-from kmean import movies_meta_data
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import sklearn
 from sklearn import tree
 from sklearn import preprocessing
@@ -19,20 +20,29 @@ def dt_all_attributes(dataset):
 	y = dataset.iloc[:, -1].values
 
 	# Create training and testing dataset
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, shuffle=True, random_state = 0)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, shuffle=True, random_state = 0)
 
-	dt = tree.DecisionTreeClassifier(max_depth=8, min_samples_split=15)
+	# cv_scores = []
+	# for i in range(2, 100):
+	# 	dt = tree.DecisionTreeClassifier(criterion='entropy',  max_depth=5, min_samples_split = i)
+	# 	dt.fit(X_train, y_train)
+	# 	y_pred = dt.predict(X_test)
+	# 	cv_scores.append(np.average(cross_val_score(dt, X, y, cv=10)))
+
+	# plt.plot(range(2, 100), cv_scores)
+	# plt.xlabel("min_samples_split of decision tree")
+	# plt.ylabel("Cross Validation Accuracy")
+	# plt.show()
+	dt = tree.DecisionTreeClassifier(criterion='entropy',  max_depth=5, min_samples_split = 35)
 	dt.fit(X_train, y_train)
 	y_pred = dt.predict(X_test)
-
-	print(accuracy_score(y_test, y_pred)) 
-
 	cv_score = cross_val_score(dt, X_train, y_train, cv=10)
-	# print("Accuracy of Decision Tree classifier after cross validation: ")
-	# print("Accuracy: %0.2f (+/- %0.2f)" % (cv_score.mean(), cv_score.std() * 2))
+	print(np.average(cv_score))
+	
 
 def dt_selected_attributes(dataset):
-	X = dataset.loc[:, ['casts_vote_score', 'keywords_vote_score', 'keywords_popularity_score', 'directors_vote_score', 'directors_popularity_score', 'casts_roi_score', 'directors_roi_score']]
+	# X = dataset.loc[:, ['casts_vote_score', 'keywords_vote_score', 'keywords_popularity_score', 'directors_vote_score', 'directors_popularity_score', 'casts_roi_score', 'directors_roi_score']]
+	X = dataset.loc[:, ['budget','runtime','casts_popularity_score','casts_vote_score'] ]
 	y = dataset.iloc[:, -1].values
 
 	# Create training and testing dataset
@@ -42,12 +52,10 @@ def dt_selected_attributes(dataset):
 	dt.fit(X_train, y_train)
 	y_pred = dt.predict(X_test)
 
-	print(accuracy_score(y_test, y_pred)) 
-
+	# print(accuracy_score(y_test, y_pred)) 
 	cv_score = cross_val_score(dt, X_train, y_train, cv=10)
-	# print("Accuracy of Decision Tree classifier (with selected attributes) after cross validation: ")
-	# print("Accuracy: %0.2f (+/- %0.2f)" % (cv_score.mean(), cv_score.std() * 2))
-
+	print(np.average(cv_score))
+	
 	dot_data = tree.export_graphviz(dt, out_file=None) 
 	graph = graphviz.Source(dot_data) 
 	graph.render("movie_tree") 
--- random_forest.py
+++ random_forest.py
@@ -7,6 +7,7 @@ Created on Thu Apr 16 14:14:24 2020
 """
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
@@ -21,17 +22,28 @@ p_dataset = pd.read_csv('data/movies_meta_data_after_processing_percentile_4_lab
 p_dataset = p_dataset.drop(columns=['return_on_investment'])
 
 def rf_all_attributes(dataset):
-    X = dataset.iloc[:, 0:11]
-    y = dataset.iloc[:, 12].values
+    X = dataset.iloc[:, 0:13]
+    y = dataset.iloc[:, -1].values
 
     # Create training and testing dataset
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, shuffle=True, random_state = 0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, shuffle=True, random_state = 0)
+    cv_scores = []
+    # for i in range(1, 100):
+    #     rf = RandomForestClassifier(n_estimators=i, min_samples_leaf = 1, criterion='entropy', random_state=0)
+    #     rf.fit(X_train, y_train)
+    #     y_pred = rf.predict(X_test)
 
-    rf = RandomForestClassifier(n_estimators=19, criterion='entropy', random_state=0)
+    #     cv_scores.append(np.average(cross_val_score(rf, X, y, cv=10)))
+
+    # plt.plot(range(1, 100), cv_scores)
+    # plt.xlabel("Number of n_estimators in Random Forest")
+    # plt.ylabel("Cross Validation Accuracy")
+    # plt.show()
+
+    rf = RandomForestClassifier(n_estimators=50, min_samples_leaf=1, criterion='entropy', random_state=0)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_test)
 
-    # print("Accuracy of RandomForestClassifier after cross validation: ")
     print(np.average(cross_val_score(rf, X, y, cv=10)))
 
 rf_all_attributes(c_dataset)
@@ -39,7 +51,7 @@ rf_all_attributes(c_dataset)
 
 def rf_selected_attributes(dataset):
     X = dataset.loc[:, ['budget','runtime','casts_popularity_score','casts_vote_score']]
-    y = dataset.iloc[:, 12].values
+    y = dataset.iloc[:, -1].values
 
     # Create training and testing dataset
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, shuffle=True, random_state = 0)
