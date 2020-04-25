df50aaaad50ec12e2158eb6b726e1176913c7eaa
xliu16 <32623183+xliu16@users.noreply.github.com>
2020-04-24 23:02:01 -0400

    changed the way of doing cv

--- decision_tree.py
+++ decision_tree.py
@@ -5,8 +5,9 @@ import sklearn
 from sklearn import tree
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, cohen_kappa_score
 from sklearn.model_selection import cross_val_score
+import skll
 import graphviz
 
 dataset = pd.read_csv('data/movies_meta_data_after_processing_with_4_cluster_label.csv').drop(columns=['return_on_investment'])
@@ -14,13 +15,21 @@ p_dataset = pd.read_csv('data/movies_meta_data_after_processing_percentile_4_lab
 
 
 def dt_all_attributes(dataset):
-	X = dataset.iloc[:, 0:-3]
+	X = dataset.iloc[:, 0:-1]
 	y = dataset.iloc[:, -1].values
 
 	# Create training and testing dataset
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, shuffle=True, random_state = 0)
-	X_test.drop(columns=['genres_roi_score', 'keywords_roi_score', 'casts_roi_score', 'directors_roi_score'])
-	# cv_scores = []
+	scores = []
+	for i in range(10):
+		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, shuffle=True)
+		X_test.drop(columns=['genres_roi_score', 'keywords_roi_score', 'casts_roi_score', 'directors_roi_score', 'popularity','vote_average'])
+	
+		dt = tree.DecisionTreeClassifier(criterion='entropy',  max_depth=5, min_samples_split = 35)
+		dt.fit(X_train, y_train)
+		y_pred = dt.predict(X_test)
+	
+		scores.append(accuracy_score(y_test, y_pred))
+	print(np.average(scores))	
 	# for i in range(2, 100):
 	# 	dt = tree.DecisionTreeClassifier(criterion='entropy',  max_depth=5, min_samples_split = i)
 	# 	dt.fit(X_train, y_train)
@@ -31,37 +40,45 @@ def dt_all_attributes(dataset):
 	# plt.xlabel("min_samples_split of decision tree")
 	# plt.ylabel("Cross Validation Accuracy")
 	# plt.show()
-	dt = tree.DecisionTreeClassifier(criterion='entropy',  max_depth=5, min_samples_split = 35)
-	dt.fit(X_train, y_train)
-	y_pred = dt.predict(X_test)
-	cv_score = cross_val_score(dt, X_train, y_train, cv=10)
-	print(np.average(cv_score))
-	
-
-def dt_selected_attributes(dataset):
-	# X = dataset.loc[:, ['casts_vote_score', 'keywords_vote_score', 'keywords_popularity_score', 'directors_vote_score', 'directors_popularity_score', 'casts_roi_score', 'directors_roi_score']]
-	X = dataset.loc[:, ['budget','runtime','casts_popularity_score','casts_vote_score'] ]
-	y = dataset.iloc[:, -1].values
-
-	# Create training and testing dataset
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, shuffle=True, random_state = 0)
-
-	dt = tree.DecisionTreeClassifier(max_depth=8, min_samples_split=15)
-	dt.fit(X_train, y_train)
-	y_pred = dt.predict(X_test)
-
-	# print(accuracy_score(y_test, y_pred)) 
-	cv_score = cross_val_score(dt, X_train, y_train, cv=10)
-	print(np.average(cv_score))
-	
-	dot_data = tree.export_graphviz(dt, out_file=None) 
+	dot_data = tree.export_graphviz(dt, out_file=None, feature_names=list(X.columns), filled=True) 
 	graph = graphviz.Source(dot_data) 
 	graph.render("movie_tree") 
 
+	# print("Balanced accuracy score")
+	# print(balanced_accuracy_score(y_test, y_pred, sample_weight=None, adjusted=False))
+	# print("cohen_kappa_score")
+	# print(cohen_kappa_score(y_test, y_pred, weights='quadratic'))
+
+def dt_original_attributes(dataset):
+	# X = dataset.loc[:, ['casts_vote_score', 'keywords_vote_score', 'keywords_popularity_score', 'directors_vote_score', 'directors_popularity_score', 'casts_roi_score', 'directors_roi_score']]
+	X = dataset.loc[:, ['budget','runtime', 'release_year', 'release_month', 'popularity','vote_average']]
+	y = dataset.iloc[:, -1].values
+	# Create training and testing dataset
+	scores = []
+	for i in range(10):
+		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, shuffle=True)
+		X_test.drop(columns=['popularity','vote_average'])
+
+		dt = tree.DecisionTreeClassifier(max_depth=8, min_samples_split=15)
+		dt.fit(X_train, y_train)
+		y_pred = dt.predict(X_test)
+		scores.append(accuracy_score(y_test, y_pred))
+
+	print(np.average(scores))
+
+	# print("Balanced accuracy score")
+	# print(balanced_accuracy_score(y_test, y_pred, sample_weight=None, adjusted=False))
+	# print("cohen_kappa_score")
+	# print(cohen_kappa_score(y_test, y_pred, weights='quadratic'))
+
+	# dot_data = tree.export_graphviz(dt, out_file=None) 
+	# graph = graphviz.Source(dot_data) 
+	# graph.render("movie_tree") 
+
 dt_all_attributes(dataset)
-dt_selected_attributes(dataset)
+dt_original_attributes(dataset)
 
 print("Percentile labels")
 dt_all_attributes(p_dataset)
-dt_selected_attributes(p_dataset)
+dt_original_attributes(p_dataset)
 
--- random_forest.py
+++ random_forest.py
@@ -8,10 +8,13 @@ Created on Thu Apr 16 14:14:24 2020
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, cohen_kappa_score
 from sklearn.model_selection import cross_val_score
+import skll
+import graphviz
 
 # Separate the dependent and independent variables
 c_dataset = pd.read_csv('data/movies_meta_data_after_processing_with_4_cluster_label.csv')
@@ -20,50 +23,63 @@ p_dataset = pd.read_csv('data/movies_meta_data_after_processing_percentile_4_lab
 p_dataset = p_dataset.drop(columns=['return_on_investment'])
 
 def rf_all_attributes(dataset):
-    X = dataset.iloc[:, 0:-3]
+    X = dataset.iloc[:, 0:-1]
     y = dataset.iloc[:, -1].values
 
     # Create training and testing dataset
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, shuffle=True, random_state = 0)
-    X_test.drop(columns=['genres_roi_score', 'keywords_roi_score', 'casts_roi_score', 'directors_roi_score'])
-    cv_scores = []
-    # for i in range(1, 100):
-    #     rf = RandomForestClassifier(n_estimators=i, min_samples_leaf = 1, criterion='entropy', random_state=0)
-    #     rf.fit(X_train, y_train)
-    #     y_pred = rf.predict(X_test)
+    scores = []
+    for i in range(10):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, shuffle=True)
+        X_test.drop(columns=['genres_roi_score', 'keywords_roi_score', 'casts_roi_score', 'directors_roi_score', 'popularity','vote_average'])
+        rf = RandomForestClassifier(n_estimators=50, min_samples_leaf=1, criterion='entropy')
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_test)
+        
+        scores.append(accuracy_score(y_test, y_pred))
+    print(np.average(scores))
 
-    #     cv_scores.append(np.average(cross_val_score(rf, X, y, cv=10)))
+    # visualization - uncomment if needed
+    dot_data = tree.export_graphviz(rf.estimators_[1], out_file=None) 
+    graph = graphviz.Source(dot_data) 
+    graph.render("movie_tree_random_forest") 
 
-    # plt.plot(range(1, 100), cv_scores)
+
     # plt.xlabel("Number of n_estimators in Random Forest")
     # plt.ylabel("Cross Validation Accuracy")
     # plt.show()
 
-    rf = RandomForestClassifier(n_estimators=50, min_samples_leaf=1, criterion='entropy', random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_test)
+    
+    # print("Balanced accuracy score")
+    # print(balanced_accuracy_score(y_test, y_pred, sample_weight=None, adjusted=False))
+    # print("cohen_kappa_score")
+    # print(cohen_kappa_score(y_test, y_pred, weights='quadratic'))
 
-    print(np.average(cross_val_score(rf, X, y, cv=10)))
 
-
-
-def rf_selected_attributes(dataset):
-    X = dataset.loc[:, ['budget','runtime','casts_popularity_score','casts_vote_score']]
+def rf_original_attributes(dataset):
+    X = dataset.loc[:, ['budget','runtime', 'release_year', 'release_month', 'popularity','vote_average']]
     y = dataset.iloc[:, -1].values
 
     # Create training and testing dataset
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, shuffle=True, random_state = 0)
+    scores = []
+    for i in range(10):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, shuffle=True)
+        X_test.drop(columns=['popularity','vote_average'])
+        rf = RandomForestClassifier(n_estimators=19, criterion='entropy', random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_test)
+        scores.append(accuracy_score(y_test, y_pred))
+    print(np.average(scores))
 
-    rf = RandomForestClassifier(n_estimators=19, criterion='entropy', random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_test)
-
-    # print("Accuracy of RandomForestClassifier after cross validation: ")
-    print(np.average(cross_val_score(rf, X, y, cv=10)))
+    # # print("Squared weighted kappa accuracy original features")
+    # print(skll.metrics.kappa(y_test, y_pred, weights='quadratic', allow_off_by_one=False))
+    # print("Balanced accuracy score")
+    # print(balanced_accuracy_score(y_test, y_pred, sample_weight=None, adjusted=False))
+    # print("cohen_kappa_score")
+    # print(cohen_kappa_score(y_test, y_pred, weights='quadratic'))
 
 rf_all_attributes(c_dataset)
-rf_selected_attributes(c_dataset)
+rf_original_attributes(c_dataset)
 
 print("Percentile labels")
 rf_all_attributes(p_dataset)
-rf_selected_attributes(p_dataset)
+rf_original_attributes(p_dataset)
